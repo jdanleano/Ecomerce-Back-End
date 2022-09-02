1038403fd89b3d30f88ae51429895814dd8a1489
Ysfboi <jdanleano@gmail.com>
2022-09-02 00:43:30 -0400

    Updated models and the category routes

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Defined columns for id and tag name
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
